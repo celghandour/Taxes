f1ace04be3a5ab93043b72f1c45543aaade238cc
celghandour <elghandour.chafik@gmail.com>
2017-05-20 10:54:10 +0200

    Dao  ok

--- src/main/java/org/cer/TaxesApplication.java
+++ src/main/java/org/cer/TaxesApplication.java
@@ -1,13 +1,42 @@
 package org.cer;
 
+
+import org.cer.dao.EntrepriseRepository;
+import org.cer.dao.TaxeRepository;
+import org.cer.entities.Entreprise;
+import org.cer.entities.IR;
+import org.cer.entities.TVA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+
 @SpringBootApplication
-public class TaxesApplication {
+public class TaxesApplication implements CommandLineRunner {
+
+	@Autowired
+	private EntrepriseRepository entrepriseRepository;
+	@Autowired
+	private TaxeRepository taxeRepository;
+
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(TaxesApplication.class, args);
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+		Entreprise e1 =entrepriseRepository.save(new Entreprise("Pixels","compasivo7@gmail.com","Sarl Pi"));
+		Entreprise e2 =entrepriseRepository.save(new Entreprise("Pixels2","compasivo7@gmail.com","Sarl Pi2"));
+		taxeRepository.save(new TVA("TVA Habitation", new Date(),900,e1));
+		taxeRepository.save(new TVA("TVA voiture", new Date(),400,e1));
+		taxeRepository.save(new IR("IR 2016", new Date(),4500,e1));
+		taxeRepository.save(new TVA("TVA Habitation", new Date(),800,e2));
+
+
+
 	}
 }
--- src/main/java/org/cer/dao/TaxeRepository.java
+++ src/main/java/org/cer/dao/TaxeRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * Created by celghandour on 20/05/17.
  */
 public interface TaxeRepository  extends JpaRepository<Taxe,Long>{
+
 }
